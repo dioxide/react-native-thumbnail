94efac295c4269df6e00e902084d222f32a3964f
dioxide <tykingco2@gmail.com>
2018-01-11 05:43:22 +0800

    wq
    upgrade

--- ios/RNThumbnail.m
+++ ios/RNThumbnail.m
@@ -13,6 +13,60 @@
 }
 RCT_EXPORT_MODULE()
 
+
+//convert mov file to mp4
+RCT_EXPORT_METHOD(convert:(NSString *)movPath resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+    @try {
+        movPath = [movPath stringByReplacingOccurrencesOfString:@"file://"
+                                                     withString:@""];
+NSLog(@"videopath of your mov file = %@",movPath);
+        AVURLAsset *avAsset = [AVURLAsset URLAssetWithURL:[NSURL fileURLWithPath:movPath] options:nil];
+        NSArray *compatiblePresets = [AVAssetExportSession exportPresetsCompatibleWithAsset:avAsset];
+
+        if ([compatiblePresets containsObject:AVAssetExportPresetLowQuality])
+        {
+            AVAssetExportSession *exportSession = [[AVAssetExportSession alloc]initWithAsset:avAsset presetName:AVAssetExportPresetPassthrough];
+            // save to temp directory
+
+            NSString* tempDirectory = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory,NSUserDomainMask,YES) lastObject];
+            NSLog(@"stating convert %@",tempDirectory);
+            NSString *videoPath = [tempDirectory stringByAppendingPathComponent: [NSString stringWithFormat:@"%@.mp4", [[NSProcessInfo processInfo] globallyUniqueString]]];
+
+            exportSession.outputURL = [NSURL fileURLWithPath:videoPath];
+            NSLog(@"videopath of your mp4 file = %@",videoPath);  // PATH OF YOUR .mp4 FILE
+            exportSession.outputFileType = AVFileTypeMPEG4;
+
+            [exportSession exportAsynchronouslyWithCompletionHandler:^{
+
+                switch ([exportSession status]) {
+
+                    case AVAssetExportSessionStatusFailed:
+                        reject([[exportSession error] localizedDescription], nil, nil);
+                        break;
+
+                    case AVAssetExportSessionStatusCancelled:
+                        reject(@"Canceled", nil, nil);
+                        break;
+
+                    case AVAssetExportSessionStatusCompleted:
+                        resolve(@{ @"path": videoPath });
+                        break;
+
+                    default:
+                        break;
+                }
+            }];
+
+        }
+    } @catch(NSException *e) {
+        reject(e.reason, nil, nil);
+    }
+}
+
+
+
 //按比例缩放,size 是你要把图显示到 多大区域 CGSizeMake(300, 140)
 -(UIImage *) imageCompressForSize:(UIImage *)sourceImage targetSize:(CGSize)size{
     UIImage *newImage = nil;
